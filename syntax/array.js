2b046cdeadbb017c669f1b2b247175440605b9c8
DESKTOP-52RNBRF\Thomas <a8484149@gmail.com>
2018-08-24 17:06:27 +0900

    배열 생성 하고 출력하기

---
+++ syntax/array.js
@@ -0,0 +1,10 @@
+// 배열 생성 하고 출력하기
+
+var arr =['A','B','C','D'];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
