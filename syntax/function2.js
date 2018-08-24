264dd6abbc720bf97b9a217e973416d68a1de89d
DESKTOP-52RNBRF\Thomas <a8484149@gmail.com>
2018-08-24 17:21:03 +0900

    함수에서 매개변수와 리턴 사용하기

---
+++ syntax/function2.js
@@ -0,0 +1,15 @@
+// 함수에서 매개변수와 리턴 사용하기
+// 함수에서 return을 시키면 함수가 끝남과 동시에 return 뒤에 있는 값을 반환한다.
+
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second) { //parameter
+    console.log('a');
+    return first + second;
+    console.log('b'');
+}
+
+console.log(sum(2,4)); // argument
+
+console.log(Math.round(1.6));
