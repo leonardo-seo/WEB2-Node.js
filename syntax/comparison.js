3a5990fd0e2a105a96af7afbf68ff3b071658e06
DESKTOP-52RNBRF\Thomas <a8484149@gmail.com>
2018-08-24 16:47:55 +0900

    비교 연산자 true false 확인하기

---
+++ syntax/comparison.js
@@ -0,0 +1,9 @@
+// 비교 연산자
+// true false 확인
+
+console.log(1 == 1); //true
+console.log(1 == 2); //false
+console.log(1 > 2); //false
+console.log(1 < 2); //false
+console.log(1 === 1); //true
+console.log(1 === 2); //false
