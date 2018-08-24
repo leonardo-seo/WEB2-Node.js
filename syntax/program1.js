c5a3502b5175a81f3bf589470737b2ea258eb36c
DESKTOP-52RNBRF\Thomas <a8484149@gmail.com>
2018-08-24 16:48:33 +0900

    제어문이 필요한 상황

---
+++ syntax/program1.js
@@ -0,0 +1,7 @@
+// 제어문이 필요한 상황
+// 'ABC1D'
+
+console.log('A');
+console.log('B');
+console.log('C1');
+console.log('D');
---
+++ syntax/program2.js
@@ -0,0 +1,7 @@
+// 제어문이 필요한 상황
+// 'ABC2D'
+
+console.log('A');
+console.log('B');
+console.log('C2');
+console.log('D');
